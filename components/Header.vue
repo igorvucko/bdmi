cc842afe7538a6248326f7cab1f58f124caaf7e4
igorvucko <igorhefner@gmail.com>
2023-05-12 12:34:23 +0200

    header count radi, wish radi, ali poboljšati ću

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,35 @@
 <template>
-    <div>
-      <button @click="toggleSidebar" class="bg-blue-500 text-white p-2">
-        {{ isOpen ? 'Close' : 'Open' }} Sidebar
-      </button>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    props: ['isOpen'],
-    methods: {
-      toggleSidebar() {
-        this.$emit('toggleSidebar')
-      }
-    },
-      computed: {
-      wishlistCount() {
-        return this.$store.getters['wishlist/wishlistCount'];
+  <div>
+    <button @click="toggleSidebar" class="bg-blue-500 text-white p-2">
+      {{ isOpen ? 'Close' : 'Open' }} Sidebar
+    </button>
+    <span class="ml-2">{{ wishlistCount }} movies on wishlist</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    wishlistCount() {
+      if (process.client) {
+        const wishlist = localStorage.getItem('wishlist');
+        const movies = wishlist ? JSON.parse(wishlist) : [];
+        return movies.length;
+      } else {
+        return 0;
       }
     }
+  },
+  methods: {
+    toggleSidebar() {
+      this.$emit('toggleSidebar');
+    }
   }
-  </script>
-  
+};
+</script>
--- pages/wishlist.vue
+++ pages/wishlist.vue
@@ -1,31 +1,26 @@
 <template>
   <div>
     <h1>Wishlist</h1>
-    <div v-if="wishlist.length === 0">
-      <p>Your wishlist is empty.</p>
-    </div>
-    <div v-else>
-      <ul>
-        <li v-for="movie in wishlist" :key="movie.id">
-          <MovieCard :movie="movie" />
-        </li>
-      </ul>
-    </div>
+    <MovieCard v-for="movie in wishlist" :key="movie.id" :movie="movie" />
   </div>
 </template>
-  
-<script >
-import { mapState } from 'vuex';
+
+<script>
 import MovieCard from '~/components/MovieCard.vue';
 
 export default {
   components: {
-    MovieCard,
+    MovieCard
   },
   computed: {
-    ...mapState({
-      wishlist: state => state.wishlist,
-    }),
-  },
+    wishlist() {
+      if (process.client) {
+        const wishlist = localStorage.getItem('wishlist');
+        return wishlist ? JSON.parse(wishlist) : [];
+      } else {
+        return [];
+      }
+    }
+  }
 };
 </script>
