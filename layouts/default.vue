7ded27cbdfee8a7f2d3adb9ba265faf4df87de89
igorvucko <igorhefner@gmail.com>
2023-05-12 13:29:35 +0200

    loading radi

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div v-if="isLoading" class="loading-container">
+      <div class="loading-indicator">
+        PRIČEKAJ SINE MOJ...
+      </div>
+    </div>
+
     <Header :isOpen="sidebarOpen" @toggleSidebar="toggleSidebar" />
     <div class="flex">
       <Sidebar v-model="sidebarOpen" />
@@ -9,6 +15,28 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.loading-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+}
+
+.loading-indicator {
+  font-size: 32px;
+  font-weight: bold;
+  background-color: white;
+  padding: 20px;
+}
+</style>
 
 <script>
 import Header from '~/components/Header.vue'
@@ -21,8 +49,15 @@ export default {
   },
   data() {
     return {
-      sidebarOpen: false
+      sidebarOpen: false,
+      isLoading: true // Set the initial value to true
     }
+  },
+  created() {
+    // Simulate loading delay
+    setTimeout(() => {
+      this.isLoading = false; // Set isLoading to false to hide the loading indicator
+    }, 2000);
   },
   methods: {
     toggleSidebar() {
