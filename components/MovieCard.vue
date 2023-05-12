7db99f9a741224e4d48903fe7ffc323a13adad75
igorvucko <igorhefner@gmail.com>
2023-05-12 12:15:23 +0200

    add,remove rade, sada se treba na wishlist spremit

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -6,7 +6,7 @@
     </div>
     <nuxt-link :to="`/movies/${movie.id}`" class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">Details</nuxt-link>
     <button @click="toggleWishlist" class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">
-      {{ isOnWishlist(movie) ? 'Remove from Wishlist' : 'Add to Wishlist' }}
+      {{ isOnWishlist ? 'Remove from Wishlist' : 'Add to Wishlist' }}
     </button>
   </div>
 </template>
@@ -19,24 +19,31 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      isOnWishlist: false
+    };
+  },
+  created() {
+    this.isOnWishlist = this.checkWishlist();
+  },
   methods: {
     toggleWishlist() {
       const wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
       const index = wishlist.findIndex((item) => item.id === this.movie.id);
 
       if (index !== -1) {
-        // Movie is already in the wishlist, so remove it
         wishlist.splice(index, 1);
       } else {
-        // Movie is not in the wishlist, so add it
         wishlist.push(this.movie);
       }
 
       localStorage.setItem('wishlist', JSON.stringify(wishlist));
+      this.isOnWishlist = this.checkWishlist();
     },
-    isOnWishlist(movie) {
+    checkWishlist() {
       const wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
-      return wishlist.some((item) => item.id === movie.id);
+      return wishlist.some((item) => item.id === this.movie.id);
     }
   }
 };
