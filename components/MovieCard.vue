4f56158715608cf0932463b6a3b82007d237f1e1
igorvucko <igorhefner@gmail.com>
2023-05-12 13:12:52 +0200

    stil dobar

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -4,8 +4,9 @@
       <h2 class="text-xl font-bold">{{ movie.title }}</h2>
       <p class="text-gray-500">{{ movie.description }}</p>
     </div>
-    <nuxt-link :to="`/movies/${movie.id}`" class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">Details</nuxt-link>
-    <button @click="toggleWishlist" class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">
+    <nuxt-link :to="`/movies/${movie.id}`" class="ml-4 px-4 py-2 rounded bg-green-500 text-white">Details</nuxt-link>
+    <button @click="toggleWishlist" class="ml-4 px-4 py-2 rounded"
+      :class="{ 'bg-red-500 text-white': isOnWishlist, 'bg-blue-500 text-white': !isOnWishlist }">
       {{ isOnWishlist ? 'Remove from Wishlist' : 'Add to Wishlist' }}
     </button>
   </div>
