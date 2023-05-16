7212f7f35ddcad3ec2e8a17147c0ff779614409d
igorvucko <igorhefner@gmail.com>
2023-05-16 12:19:46 +0200

    link123

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -5,17 +5,7 @@
         <li>Link 1</li>
         <li>Link 2</li>
         <li>Link 3</li>
-        <li>Link 4</li>
-        <li>Link 5</li>
-        <li>Link 6</li>
-        <li>Link 7</li>
-        <li>Link 8</li>
-        <li>Link 9</li>
-        <li>Link 10</li>
-        <li>Link 11</li>
-        <li>Link 12</li>
-        <li>Link 13</li>
-        <li>Link 14</li>
+        
       </ul>
     </div>
   </transition>
