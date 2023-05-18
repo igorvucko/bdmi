09e1efe3b39c248c26243ff578a0cb7ead3e3c28
igorvucko <igorhefner@gmail.com>
2023-05-18 15:29:23 +0200

    v-data-table

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -4,8 +4,8 @@
       <h2 class="text-xl font-bold">{{ movieData.title }}</h2>
       <p class="text-gray-500">{{ movieData.description }}</p>
     </div>
-    <router-link :to="`/movies/${movieData.id}`"
-      class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">Details</router-link>
+    <v-btn dark color :to="`/movies/${movieData.id}`"
+      class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">Details</v-btn>
     <v-btn dark color="primary" @click="toggleWishlist" class="ml-4" 
       :disabled="isLoading">
       {{ isOnWishlist ? 'Remove from Wishlist' : 'Add to Wishlist' }}
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -4,39 +4,29 @@
     <div v-if="isLoading" class="loading-indicator">
       Loading...
     </div>
-    <table v-else class="data-table">
-      <!-- Table headers -->
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <!-- Table body -->
-      <tbody>
-        <tr v-for="movie in displayedMovies" :key="movie.id">
-          <td>
+    <v-card v-else class="data-table">
+      <v-card-text>
+        <v-row>
+          <v-col v-for="movie in displayedMovies" :key="movie.id" cols="6">
             <h2 class="text-xl font-bold">{{ movie.title }}</h2>
-          </td>
-          <td>
-            <router-link :to="`/movies/${movie.id}`" class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">
+            <v-btn dark color="primary" :to="`/movies/${movie.id}`" class="ml-4 px-4 py-2 rounded bg-blue-500 text-white">
               Details
-            </router-link>
-            <button @click="toggleWishlist(movie)" class="ml-4 px-4 py-2 rounded"
+            </v-btn>
+            <v-btn dark color="primary" @click="toggleWishlist(movie)" class="ml-4 px-4 py-2 rounded"
               :class="{ 'bg-red-500': isInWishlist(movie), 'bg-blue-500': !isInWishlist(movie) }" :disabled="isLoading">
               {{ isInWishlist(movie) ? 'Remove from Wishlist' : 'Add to Wishlist' }}
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
     <CustomPagination :totalPages="totalPages" :currentPage="defaultPagination" @pageChange="changePage" />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import { mapGetters, mapActions, mapState } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import CustomPagination from '@/components/CustomPagination.vue';
 
 export default {
@@ -45,10 +35,10 @@ export default {
   },
   data() {
     return {
-      movies: [],
+      movies: undefined,
       isLoading: true,
       defaultPagination: 1,
-      moviesPerPage: 5,
+      moviesPerPage: 6,
     };
   },
   computed: {
@@ -56,31 +46,41 @@ export default {
     displayedMovies() {
       const start = (this.defaultPagination - 1) * this.moviesPerPage;
       const end = start + this.moviesPerPage;
-      return this.movies.slice(start, end);
+      const sortedMovies = this.movies ? this.movies.slice(start, end) : [];
+
+      // Sort movies by title in ascending order
+      sortedMovies.sort((a, b) => {
+        const titleA = a.title.toLowerCase();
+        const titleB = b.title.toLowerCase();
+        if (titleA < titleB) return -1;
+        if (titleA > titleB) return 1;
+        return 0;
+      });
+
+      return sortedMovies;
     },
     totalPages() {
-      return Math.ceil(this.movies.length / this.moviesPerPage);
+      return this.movies ? Math.ceil(this.movies.length / this.moviesPerPage) : 0;
+    },
+    headers() {
+      return [
+        { text: 'Title', value: 'title' },
+        { text: 'Actions', value: '', sortable: false },
+      ];
     },
   },
   created() {
     this.fetchMovies();
   },
   methods: {
-    ...mapActions('movies', ['fetchMovies']),
-    addToWishlist(movie) {
-      this.$store.dispatch('wishlist/addToWishlist', movie);
-    },
-    removeFromWishlist(movie) {
-      this.$store.dispatch('wishlist/removeFromWishlist', movie);
-    },
+    ...mapActions('wishlist', ['addToWishlist', 'removeFromWishlist']),
     toggleWishlist(movie) {
-  if (this.isInWishlist(movie)) {
-    this.removeFromWishlist(movie);
-  } else {
-    this.addToWishlist(movie);
-  }
-},
-
+      if (this.isInWishlist(movie)) {
+        this.removeFromWishlist(movie);
+      } else {
+        this.addToWishlist(movie);
+      }
+    },
     changePage(pageNumber) {
       this.defaultPagination = pageNumber;
     },
@@ -98,19 +98,13 @@ export default {
       }
     },
   },
-};
+}
 </script>
 
 <style>
 .data-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-.data-table th,
-.data-table td {
-  padding: 8px;
-  border: 1px solid #ccc;
+  margin: 0 auto;
+  max-width: 1200px;
 }
 
 .text-xl {
